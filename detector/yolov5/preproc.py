2489ceefb0105c4d8bb758285a760492523b30b5
siren16 <89844201+siren16@users.noreply.github.com>
2021-09-28 07:32:30 +0900

    Add files via upload

---
+++ detector/yolov5/preproc.py
@@ -0,0 +1,10 @@
+def makePersonBox(input):
+    results = input.pandas().xyxy[0]
+    results = results[results["class"] == 0]
+    result = {}
+    for i in range(len(results)):
+        a = i + 1
+        l = results.loc[i]
+        
+        result[f"person{a}"] = {"left_bottom" : (l["xmin"], l["ymin"]), "right_bottom" : (l["xmax"], l["ymin"]), "left_top" : (l["xmin"], l["ymin"]), "right_top" : (l["xmax"],l["ymax"])}
+    return result
